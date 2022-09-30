a651fe4df1f40539ea5a820ec78b92df781fce02
German <germanm1853@gmail.com>
2022-09-30 14:03:54 -0500

    calculadora finalizada

--- main.js
+++ main.js
@@ -4,13 +4,26 @@ let btn=document.getElementById("boton");
 function calcular() {
   let peso = document.getElementById("peso").value;
   let altura = document.getElementById("altura").value;
+  let total=0
   if(altura%1==0){
     altura=altura/100;
-    let total = peso/(altura*altura);
+    total = peso/(altura*altura);
     document.getElementById("resultado").innerHTML = total.toFixed(2);
   } else{
-    let total = peso/(altura*altura);
+    total = peso/(altura*altura);
     document.getElementById("resultado").innerHTML = total.toFixed(2);
+  }
+
+  if(total>=18.5 && total<=24.9){
+    document.getElementById("resultado").style.backgroundColor="green";
+    } else if(total>=25 && total<=29.9){
+      document.getElementById("resultado").style.backgroundColor="yellow";
+    } else if(total>=30 && total<=34.9){
+      document.getElementById("resultado").style.backgroundColor="orange";
+    }else if(total>=35 && total<=39.9){
+      document.getElementById("resultado").style.backgroundColor="red";
+    }else if(total>=40){
+        document.getElementById("resultado").style.backgroundColor="red";
   }
 }
 btn.addEventListener("click",calcular);
